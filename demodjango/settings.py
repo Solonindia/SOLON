bfef92247f1f6af32cef515135f4174e5cf00c9a
Onmget <onm.get@solonindia.com>
2024-07-24 15:31:21 +0530

    setting update2

--- demodjango/settings.py
+++ demodjango/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'demodjango.wsgi.application'
 CSRF_TRUSTED_ORIGINS = [
-    'https://dgrapp-prod.azurewebsites.net',
+    'https://dgr.azurewebsites.net/home/',  # Add your production domain
 ]
 
 
